ed0eda9bf67c5d603626b8b1fd24642c54069b16
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-13 17:54:01 +0200

    refactor: separate update_correct_guesses method
    Separated the update_correct_guesses method into check_letter and
    check_word, and renamed the method to check_guess.

--- lib/logic.rb
+++ lib/logic.rb
@@ -11,32 +11,34 @@ module GameLogic
     make_guess
   end
 
-  def update_correct_guesses(guess)
+  def check_guess(guess)
     word_arr = @word.split('')
-    if guess.length == 1
-      if word_arr.include? guess
-        word_arr.each_index { |index| @correct_guesses[index] = guess if word_arr[index] == guess }
-      else
-        @incorrect_guesses << guess
-      end
+    guess.length == 1 ? check_letter(word_arr, guess) : check_word(word_arr, guess)
+  end
+
+  def check_letter(word_arr, guess)
+    if word_arr.include? guess
+      word_arr.each_index { |index| @correct_guesses[index] = guess if word_arr[index] == guess }
     else
-      guess_arr = guess.split('')
-      guess_arr == word_arr ? @correct_guesses = guess_arr : @incorrect_guesses << guess
+      @incorrect_guesses << guess
     end
+  end
+
+  def check_word(word_arr, guess)
+    guess_arr = guess.split('')
+    guess_arr == word_arr ? @correct_guesses = guess_arr : @incorrect_guesses << guess
   end
 
   def play
-    @guess_count.times do |guess_num|
-      puts 'Enter your guess>>'
-      guess = make_guess
-      update_correct_guesses(guess)
-      show_game_state
+    puts "\nGuesses remaining: #{@guess_count}; Enter your guess>>"
+    guess = make_guess
+    check_guess(guess)
+    show_game_state
 
-      break if win_game?(guess) || lose_game?
-    end
+    play unless win_game? || lose_game? || @guess_count < 1
   end
 
-  def win_game?(guess)
+  def win_game?
     @correct_guesses.none? { |char| char == '_' }
   end
 
