0c18447c3a5679dc6eb45e09a789154af8a0fff9
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-13 18:39:25 +0200

    feat: add serialization methods

--- lib/main.rb
+++ lib/main.rb
@@ -1,13 +1,16 @@
 require_relative 'logic'
+require 'json'
 
 class Hangman
+  attr_accessor :guess_count, :word, :correct_guesses, :incorrect_guesses
+
   include GameLogic
 
-  def initialize(guess_count, word)
+  def initialize(guess_count, word, correct_guesses = nil, incorrect_guesses = nil)
     @guess_count = guess_count
     @word = word
-    @correct_guesses = Array.new(@word.length, '_')
-    @incorrect_guesses = []
+    @correct_guesses = correct_guesses || Array.new(@word.length, '_')
+    @incorrect_guesses = incorrect_guesses || []
   end
 
   def self.random_word
@@ -20,5 +23,30 @@ class Hangman
     dictionary.close
 
     dictionary_array.sample
+  end
+
+  def to_json
+    json_data = {
+      guess_count: @guess_count,
+      word: @word,
+      correct_guesses: @correct_guesses,
+      incorrect_guesses: @incorrect_guesses
+    }
+
+    File.open('../save_data.json', 'w') do |file|
+      file.puts JSON.dump(json_data)
+    end
+  end
+
+  def self.from_json
+    begin
+      json_string = File.read('../save_data.json')
+    rescue Errno::ENOENT
+      puts 'No save data to load from.'
+      return
+    end
+
+    data = JSON.parse(json_string, symbolize_names: true)
+    self.new(data[:guess_count], data[:word], data[:correct_guesses], data[:incorrect_guesses])
   end
 end
