28d2c505d4a4ec2c40053ec153600bad46fe5ac9
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-13 19:55:36 +0200

    feat: add game_message method

--- lib/display.rb
+++ lib/display.rb
@@ -3,4 +3,16 @@ module Display
     print "\nWord: #{@correct_guesses.join}"
     puts "  Incorrect guesses: #{@incorrect_guesses.join(', ')}\n"
   end
+
+  def game_message(type)
+    {
+      'save prompt' => "Would you like to save the current session? Enter 'y' or 'n'>>",
+      'saved' => 'Current session has been saved.',
+      'load prompt' => "Would you like to load the previous session? Enter 'y' or 'n'>>",
+      'loaded' => 'Previous session has been loaded.',
+      'quit' => 'Exiting...',
+      'win' => 'Congratulations! You guessed the word and survived!',
+      'lose' => 'Game over... You couldn\'t guess the word and the stickman was hanged.'
+    }
+  end
 end
