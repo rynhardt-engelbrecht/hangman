798c4436c99e1d6a6767e95a46e4946b4513aaf8
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-05-14 10:28:14 +0200

    feat: add control_input method
    Added the control_input method to allow the user to save the current
    sessio, to load a previous session, or to quit the game entirely.

--- lib/logic.rb
+++ lib/logic.rb
@@ -6,20 +6,30 @@ require_relative 'display'
 module GameLogic
   # include Display
 
-  EXIT_KEYS = %w[q; exit;]
-  SAVE_KEYS = %w[s; save;]
-  LOAD_KEYS = %w[l; load;]
-  CONTROL_KEYS = EXIT_KEYS.concat(SAVE_KEYS, LOAD_KEYS)
+  EXIT_KEYS = %w[q; exit;].freeze
+  SAVE_KEYS = %w[s; save;].freeze
+  LOAD_KEYS = %w[l; load;].freeze
+  CONTROL_KEYS = [].concat(EXIT_KEYS, SAVE_KEYS, LOAD_KEYS).freeze
 
   def user_input
     input = gets.chomp.downcase
-
-    if CONTROL_KEYS.include? input
-      EXIT_KEYS.include?(input) ? return : SAVE_KEYS.include?(input) ? to_json : self.class.from_json
+    if CONTROL_KEYS.include?(input)
+      control_input(input)
     else
       return evaluate_guess(key, input) if [1, key.length].include?(input.length) && /^[[:alpha:]]+$/.match?(input)
-      puts "Invalid input."
-      user_input
+    end
+
+    puts 'Invalid input.'
+    user_input
+  end
+
+  def control_input(input)
+    if EXIT_KEYS.include?(input)
+      exit
+    elsif SAVE_KEYS.include?(input)
+      to_json
+    else
+      self.class.from_json
     end
   end
 
@@ -27,19 +37,19 @@ module GameLogic
     temp_key = key_word.split('')
     return check_letter(temp_key, string) if string.length == 1
 
-    self.guess_history.push(string)
-    check_word(temp_key, string)
+    guess_history.push(string)
+    check_word(string)
   end
 
   def check_letter(key_word, char)
-    if key.include?(char)
-      key_word.each_index do |index|
-        uncovered_key[index] = char if key_word[index] == char
-      end
+    return unless key.include?(char)
+
+    key_word.each_index do |index|
+      uncovered_key[index] = char if key_word[index] == char
     end
   end
 
-  def check_word(key_word, string)
-    uncovered_key = string.split('') if string == key
+  def check_word(string)
+    self.uncovered_key = string.split('') if string == key
   end
 end
